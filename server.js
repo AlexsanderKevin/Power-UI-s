3d4382be7f25302a1329e23803790f149f22cedc
kevin alexsander <kevin13348@gmail.com>
2022-02-11 13:56:35 -0300

    using express to serve

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-var http = require('http')
-var static = require('node-static')
+// var http = require('http')
+// var static = require('node-static')
 
-var path = new static.Server(`${__dirname}/app`)
+// var path = new static.Server(`${__dirname}/app`)
 
-http.createServer((req, res) => {
-    req.addListener('end', function(){
-        path.serve(req,res)
-    }).resume()
-}).listen(5505)
+// http.createServer((req, res) => {
+//     req.addListener('end', function(){
+//         path.serve(req,res)
+//     }).resume()
+// }).listen(5505)
 
-console.log('server running...')
+// console.log('server running...')
+const express = require("express")
+const app = express()
+const porta = 3001
+
+app.use(express.static('public'))
+
+app.listen(porta, () => {
+    console.log(`Running at port: ${porta}`)
+})
